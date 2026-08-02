4596a7542ad6917172c34f42c85e9afad684f765
Medicalcertificategenerator <Medicalcertificategenerator@users.noreply.github.com>
2026-08-02 13:17:25 +0530

    feat: add AdMaven pop ad, push notifications, and in-page push scripts to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -171,7 +171,14 @@ import { WebMcpProvider } from "@/components/WebMcpProvider";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head></head>
+      <head>
+        {/* AdMaven Pop Ad */}
+        <script data-cfasync="false" src="//dcbbwymp1bhlf.cloudfront.net/?wbbcd=1500345" />
+        {/* AdMaven Push Notifications */}
+        <script data-cfasync="false" src="/sw.js" />
+        {/* AdMaven In Page Push */}
+        <script data-cfasync="false" src="//dcbbwymp1bhlf.cloudfront.net/?wbbcd=1500383" />
+      </head>
       <body className={`${dmSans.variable} ${outfit.variable} antialiased`}>
         <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-C6359RT200" />
         <Script id="google-analytics" strategy="afterInteractive">
